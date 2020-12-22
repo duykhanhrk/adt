04e8aaee463083728d759936e0b9039af87ddfb1
dkhnh <tranduykhanh0424@gmail.com>
2020-12-22 13:53:36 +0700

    221220201531

--- center.py
+++ center.py
@@ -21,14 +21,37 @@ def save_config():
     pass
 
 # Database
+def packet_count(database):
+    return DATABASES[database].size()
+
+def get_packet(database, index):
+    return DATABASES[database][index]
+
+def set_packet(database, index, data):
+    DATABASES[database][index] = data
+
+def append_packet(database, packet):
+    DATABASES[database].append(packet)
+
+def database_count():
+    return DATABASES.keys().size()
+
+def database_keys():
+    return DATABASES.keys()
+
 def get_database(database):
     return database
 
 def set_database(database, data):
-    DATABASES[database] = database
+    DATABASES[database] = data
 
-def add_database(into_database, from_database):
+def append_database(database, data):
+    DATABASES[database] = data
+
+def add_databases(into_database, from_database):
     DATABASES[into_database] = DATABASES[into_database] + DATABASES[from_database]
 
-def ufity_database(into_database, from_database):
-    pass
+def ufity_databases(into_database, from_database):
+    for data in DATABASES[from_database]:
+        if not data in DATABASES[into_database]:
+            DATABASES[into_database].append(data)
