0bfc498b2b133cc32e1734d5455c5383a6b83856
duykhanhrk <tranduykhanh.rk@gmail.com>
2021-02-24 20:48:59 +0700

    Spell check

--- storage.py
+++ storage.py
@@ -5,10 +5,10 @@ import platform
 import random
 import shutil
 
-SAPERATE = "\\"
+SEPARATE = "\\"
 
 if platform.system() == "Linux":
-    SAPERATE = "/"
+    SEPARATE = "/"
 
 def current_path():
     return os.path.dirname(os.path.realpath(__file__))
@@ -50,35 +50,35 @@ def make_string_id():
 
 # ext and node
 def does_ext_exist(ext_id):
-    return does_file_exist(data_center.get_exts_folder() + SAPERATE + ext_id + ".ext_json")
+    return does_file_exist(data_center.get_exts_folder() + SEPARATE + ext_id + ".ext_json")
 
 def does_node_exist(node_id):
-    return does_file_exist(data_center.get_nodes_folder() + SAPERATE + node_id + ".node_json")
+    return does_file_exist(data_center.get_nodes_folder() + SEPARATE + node_id + ".node_json")
 
 def load_ext(ext_id):
-    return load(data_center.get_exts_folder() + SAPERATE + ext_id + ".ext_json")
+    return load(data_center.get_exts_folder() + SEPARATE + ext_id + ".ext_json")
 
 def save_ext(ext_id, data):
-    save(data_center.get_exts_folder() + SAPERATE + ext_id + ".ext_json", data)
+    save(data_center.get_exts_folder() + SEPARATE + ext_id + ".ext_json", data)
 
 def add_ext(file_path):
     ext_id = make_string_id()
     while does_ext_exist(ext_id):
         ext_id = make_string_id()
 
-    copy(file_path, data_center.get_exts_folder() + SAPERATE + ext_id + ".ext_json")
+    copy(file_path, data_center.get_exts_folder() + SEPARATE + ext_id + ".ext_json")
 
 def delete_ext(ext_id):
-    delete(data_center.get_exts_folder() + SAPERATE + ext_id + ".ext_json")
+    delete(data_center.get_exts_folder() + SEPARATE + ext_id + ".ext_json")
 
 def load_node(node_id):
-    return load(data_center.get_nodes_folder() + SAPERATE + node_id + ".node_json")
+    return load(data_center.get_nodes_folder() + SEPARATE + node_id + ".node_json")
 
 def save_node(node_id, data):
-    save(data_center.get_nodes_folder() + SAPERATE + node_id + ".node_json", data)
+    save(data_center.get_nodes_folder() + SEPARATE + node_id + ".node_json", data)
 
 def delete_node(node_id):
-    delete(data_center.get_nodes_folder() + SAPERATE + node_id + ".node_json")
+    delete(data_center.get_nodes_folder() + SEPARATE + node_id + ".node_json")
 
 def is_true_ext_id(ext_id):
     chrs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
