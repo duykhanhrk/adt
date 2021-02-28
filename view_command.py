a969811a792d30a7560ab0d3b4471bff3c576d4a
duykhanhrk <tranduykhanh.rk@gmail.com>
2021-02-28 18:54:27 +0700

    Fix view command

--- view_command.py
+++ view_command.py
@@ -1,19 +1,34 @@
 import data_center
 import message
 
-def view_role(data, end_with = "\n"):
+def view_role(data, end_with = "\n", after_print = None, before_print = None):
     if type(data) == list:
+        # before print
+        if before_print != None:
+            print(before_print, end=end_with)
+
+        # print
         for item in data:
-            if type(item) == dict or type(item) == list:
+            if type(item) == dict:
                 print("")
                 view_role(item)
                 print("")
+            elif type(item) == list:
+                view_role(item, before_print="\n", after_print="\b\b ", end_with="")
+                print("")
             else:
                 view_role(item, end_with=", ")
+
+        # after print
+        if after_print != None:
+            print(after_print, end=end_with)
     elif type(data) == dict:
         for key in data.keys():
             print(key, end = ": ")
-            view_role(data[key])
+            if type(data[key]) == list:
+                view_role(data[key], after_print="\b\b ")
+            else:
+                view_role(data[key])
     else:
         print(data, end=end_with)
 
